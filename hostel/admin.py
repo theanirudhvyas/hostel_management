a713e89709208841c4eeffa232300bd54f2af2c1
Anirudh <anirudh2403@gmail.com>
2016-02-23 18:02:20 +0530

    recognized admin and students on login!

--- hostel/admin.py
+++ hostel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from hostel.models import Diff, Student
+from hostel.models import Diff, Student, Room
 # Register your models here.
 admin.site.register(Diff)
 admin.site.register(Student)
+admin.site.register(Room)
